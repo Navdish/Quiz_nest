5db22ad368dce1cfaeb84ffd8b6dd568af10c81d
Navdish1152 <navdish.1152@zenmonk.tech>
2024-10-07 18:36:33 +0530

    migration running

---
+++ src/features/question/question.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+// import {  } from 'src/infrastructure/repositories/question/question.repository';
+
+@Module({
+    controllers: [],
+    providers: [
+        
+    ],
+})
+export class AdvisorModule {}
